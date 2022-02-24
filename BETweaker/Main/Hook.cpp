47fefb1d56fe9690a2f565ffcb19c86375738af0
dofes <91889957+dofes@users.noreply.github.com>
2022-02-25 00:34:31 +0800

    农作物发射器种植判断

--- BETweaker/Main/Hook.cpp
+++ BETweaker/Main/Hook.cpp
@@ -23,9 +23,12 @@ THook(void, "?ejectItem@DispenserBlock@@IEBAXAEAVBlockSource@@AEBVVec3@@EAEBVIte
      BlockSource* a2, Vec3* a3, FaceID a4,ItemStack* a5, Container* a6,unsigned int a7) {
     auto pos = a3->toBlockPos();
     if (a5->getItem()->isSeed()) {
-        if (a2->getBlock(pos.add(0, -1, 0)).getTypeName() == VanillaBlocks::mFarmland->getTypeName()) {
+        if (a2->getBlock(pos.add(0, -1, 0)).getTypeName() == VanillaBlocks::mFarmland->getTypeName() &&
+            a2->getBlock(pos).getTypeName() == "minecraft:air") {
             auto out = dAccess<Block*, 8>(dAccess<SeedItemComponentLegacy*, 488>(a5->getItem()));
             Level::setBlock(pos, a2->getDimensionId(), out);
+            a5->remove(1);
+            return ;
         }
     }
     return original(a1, a2, a3, a4, a5, a6, a7);
